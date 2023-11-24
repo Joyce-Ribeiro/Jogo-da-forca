d0e314d542f77ea6cfacc4abd17c951966568694
Joyce-Ribeiro <joyribeirogxavier@gmail.com>
2023-11-24 20:52:01 -0300

    voltando commit

---
+++ forca.py
@@ -0,0 +1,17 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+        print("Jogando...")
+        
+if(__name__ == "__main__"):
+    jogar()
