b7e2fafcb419589643b1b52de26ef3e4e38ca5f8
Joyce-Ribeiro <joyribeirogxavier@gmail.com>
2023-11-24 21:15:50 -0300

    adicionando leitura do arquivo de palavras

--- forca.py
+++ forca.py
@@ -1,4 +1,17 @@
+import random
 def jogar():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
